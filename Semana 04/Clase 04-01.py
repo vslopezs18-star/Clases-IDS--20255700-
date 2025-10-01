6e1ace0f8f5b79640cff9df367a0ea3a9489e7d6
vslopezs18-star <vslopezs18@gmail.com>
2025-10-01 11:56:08 -0600

    Update Clase 04-01.py

--- Semana 04/Clase 04-01.py
+++ Semana 04/Clase 04-01.py
@@ -16,5 +16,8 @@ media_edad = 18.23
 print(type(cantidad_alumunos))
 print(type(media_edad))
 
-print(type(cantidad_alumunos)is int)
+print(type(cantidad_alumunos)is int) # no es lo mismo "int" o sea no le pongas comillas, porque sino quiere decir que es literalmente int y no queres eso
 print(type(media_edad)is int)
+
+print("el usuario es" , "Alvin" , "y tiene" , "74" , "pajaritos en su aula")
+print("el usario es", usuario , "y tiene" , "74" , "pajaritos en su aula") # Podes ponerle simplemente usuario y entiende porque ya puse la variable usuario como Alvin
