df6924171a00dde19278b0ecc32a68874895bc90
vslopezs18-star <vslopezs18@gmail.com>
2025-10-01 12:20:42 -0600

    Update Clase 04-01.py

--- Semana 04/Clase 04-01.py
+++ Semana 04/Clase 04-01.py
@@ -11,7 +11,8 @@ usuario == "Javier"
 # Presentar resumenes del ususario
 
 cantidad_alumunos = 74
-media_edad = 18.23
+media_edad = 18.231234
+monto_hope = 1234.56
 
 print(type(cantidad_alumunos))
 print(type(media_edad))
@@ -20,4 +21,12 @@ print(type(cantidad_alumunos)is int) # no es lo mismo "int" o sea no le pongas c
 print(type(media_edad)is int)
 
 print("el usuario es" , "Alvin" , "y tiene" , "74" , "pajaritos en su aula")
-print("el usario es", usuario , "y tiene" , "74" , "pajaritos en su aula") # Podes ponerle simplemente usuario y entiende porque ya puse la variable usuario como Alvin
+print("el usario es", usuario , "y tiene" , cantidad_alumunos , "pajaritos en su aula") # Podes ponerle simplemente usuario y entiende porque ya puse la variable usuario como Alvin, al igual que con la edad y lo demás
+print("y la edad promedio es de" , media_edad)
+
+# F string es un texto formateado, para hacerlo solo se pone una f antes del texto
+# Los corchetes abren la posibilidad de hacer cálculos o devolver los valores de las variables
+
+print(f"El usario es {usuario}")
+print(f"y en su aula con {cantidad_alumunos - 4} pajaritos en su aula")
+print(f"con edad promedio de {media_edad: .2f} años") # después de la variable le pones dos puntos y el formato que queres, en este caso fue que quería dos decimales en vez del montón
