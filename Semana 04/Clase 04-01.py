075814603a160be4195a82d7df12e14e0a0b4bcc
vslopezs18-star <vslopezs18@gmail.com>
2025-10-01 11:49:02 -0600

    Update Clase 04-01.py

--- Semana 04/Clase 04-01.py
+++ Semana 04/Clase 04-01.py
@@ -9,3 +9,12 @@ usuario == "Javier"
 # Definir tareas para usuario
 
 # Presentar resumenes del ususario
+
+cantidad_alumunos = 74
+media_edad = 18.23
+
+print(type(cantidad_alumunos))
+print(type(media_edad))
+
+print(type(cantidad_alumunos)is int)
+print(type(media_edad)is int)
