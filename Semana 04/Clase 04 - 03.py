a10257c20e47eb8bd21d47aa9361cb9c67cb45b1
vslopezs18-star <vslopezs18@gmail.com>
2025-10-03 15:51:38 -0600

    Update Clase 04 - 03.py

--- Semana 04/Clase 04 - 03.py
+++ Semana 04/Clase 04 - 03.py
@@ -14,7 +14,7 @@ print(len(s3))"""
 # Ese es el ejemplo de cuando pones len y te va a poner los espacios
 # La cadena de caracteres es Python las interpreta como arreglos []
 
-my_string = "ABCDEFGHI" # string o cadena de carecteres
+my_string = "ABCDEFCCC" # string o cadena de carecteres
 
 """letra = int(input("Indique el indice de la letra a mostrar")) # Aquí me va a adar error en la terminal porque cada vez que se pone input lo agarra como texto, entonces para arreglarlo se pone int antes de input
 print(my_string[letra])
@@ -27,5 +27,16 @@ print(my_string[0])"""
 
 # Slices (subsettings)
 
-print(my_string[1:5]) # Los dos puntos es como decir "desde, hasta", pero el último elemento que pono en este caso 4 no será ese el último porque es exclusivo, entonces llegará hasta el tercero
+print(my_string[1:5:3]) # Los dos puntos es como decir "desde, hasta", pero el último elemento que pono en este caso 4 no será ese el último porque es exclusivo, entonces llegará hasta el tercero
+# El segundo dos puntos es para indicar los saltos, si le pondo dos es porque se va a saltar uno sí uno no
+# Índices negativos son los que empiezan desde el final de la secuencia, -1 hace referencia al útlimo elemento, -2 el penúltimoy así
 
+print("H" is my_string)
+print("O" not in my_string)
+
+print("AJP" *8) # Aquí hago una repetición
+print(min(my_string)) # min siginifica el menor
+print(max(my_string)) # max es el mayor
+print(my_string.count("C")) # aquí cuenta cuantas veces aparece el elemento que le pongo
+
+# El punto es para poner operaciones dentro del texto que se puede, como encontrar la mayúsucula y más cosas, en este caso se llama método porque a la string le aplico una "función" que se llama en realidad método
