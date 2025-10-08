53d5b1e7e7d8b3d36714e32f9e517d2ee4e90692
vslopezs18-star <vslopezs18@gmail.com>
2025-10-07 19:24:52 -0600

    .

--- Semana 05/Clase 05-01.py
+++ Semana 05/Clase 05-01.py
@@ -16,5 +16,11 @@ print(prueba==prueba[::-1]) #Esto es para verificar si es un palíndromo o no, o
 
 # Los números si los ponemos así tal cual sin comillas no tienen len, solo el texto cuando se ponen las comillas
 
-edad = input(26) # Lo que es un input es un string a pesar de que sea un número, si yo quiero que se comporte como un número le tengo que poner int antes del input
+edad = int(input()) # Lo que es un input es un string a pesar de que sea un número, si yo quiero que se comporte como un número le tengo que poner int antes del input
+# print(len(edad)) # Aquí lo va a tomar como string o sea texto porque sigue siendo texto el 26 al solo dejar input
 
+print(palabra.lower()) # El lower es un método, que hace que se haga minúscula
+print(palabra2.upper()) # Aquí será mayúscula
+print(palabra2.capitalize()) # Llevan paréntesis porque como los métodos son funciones aplicables a un objeto tienen que llevar paréntesis
+
+# La capitalize es para que la primera letra sea mayúscula
---
+++ Semana 05/input.py
@@ -0,0 +1,1 @@
+edad = input("Digite la edad:")
