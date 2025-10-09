5ba063ea70e4c5aad59240e2789467dddc939e96
vslopezs18-star <vslopezs18@gmail.com>
2025-10-09 16:15:29 -0600

    Update Clase 04 - 02.py

--- Semana 04/Clase 04 - 02.py
+++ Semana 04/Clase 04 - 02.py
@@ -1,8 +1,15 @@
-cantidad_alumnos = 25
+"""cantidad_alumnos = 25
 nombre_profe = "Alvin"
 nuevas_inscripciones = 0
 nombre_profe = input("Ingrese el nombre del profesor: ") #Este es el inverso de print, me sirve para ingresar a la pantalla
 nuevas_inscripciones = int(input("Nuevos alumnos: ")) #Para que no me de error al sumar texto con número, le puse que lo tomara como entero la cantidad de alumnos
 print(type(nombre_profe))
 print(nombre_profe)
-print(cantidad_alumnos + nuevas_inscripciones)
+print(cantidad_alumnos + nuevas_inscripciones)"""
+
+# Ejericio guía
+
+nombre_usuario = input("Por favor ingresa tu nombre: ")
+print("Bienvenido")
+edad_usuario = input("Por favor ingresa tu edad: ")
+print(edad_usuario)
