05c7edddf566163b45cb60a4a86594b0c8261c6b
vslopezs18-star <vslopezs18@gmail.com>
2025-10-10 13:37:56 -0600

    Update Control2 Ejericio 1.py

---
+++ Semana 05/Control2 Ejericio 1.py
@@ -0,0 +1,31 @@
+PLATOS_DISPONIBLES = ["Hamburguesa","Hotdog","Pizza","Tacos","Lasaña","Ensalada","Pupusas","Burrito","Alitas de pollo","Papas fritas"]
+
+Hamburguesa = int(input())
+print(f"El plato seleccionado por Alvin es: {PLATOS_DISPONIBLES[0]}")
+
+Hotdog = int(input())
+print(f"El plato seleccionado por Alvin es: {PLATOS_DISPONIBLES[1]}")
+
+Pizza = int(input())
+print(f"El plato seleccionado por Alvin es: {PLATOS_DISPONIBLES[2]}")
+
+Tacos = int(input())
+print(f"El plato seleccionado por Alvin es: {PLATOS_DISPONIBLES[3]}")
+
+Lasaña = int(input())
+print(f"El plato seleccionado por Alvin es: {PLATOS_DISPONIBLES[4]}")
+
+Ensalada = int(input())
+print(f"El plato seleccionado por Alvin es: {PLATOS_DISPONIBLES[5]}")
+
+Ensalada = int(input())
+print(f"El plato seleccionado por Alvin es: {PLATOS_DISPONIBLES[6]}")
+
+Pupusas = int(input())
+print(f"El plato seleccionado por Alvin es: {PLATOS_DISPONIBLES[7]}")
+
+Burrito = int(input())
+print(f"El plato seleccionado por Alvin es: {PLATOS_DISPONIBLES[8]}")
+
+Alitas_de_pollo = int(input())
+print(f"El plato seleccionado por Alvin es: {PLATOS_DISPONIBLES[9]}")
