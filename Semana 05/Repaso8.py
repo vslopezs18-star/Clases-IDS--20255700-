a8f345a93679d5c66d50b5238abe3d2cd203999b
vslopezs18-star <vslopezs18@gmail.com>
2025-10-10 10:11:40 -0600

    Create Repaso8.py

---
+++ Semana 05/Repaso8.py
@@ -0,0 +1,17 @@
+DIAS = ["LUNES", "MARTES", "MIERCOLES", "JUEVES", "VIERNES"]
+
+LU = int(input())
+DIAS[0] = LU
+print(f"El día LUNES se vendieron {LU} productos")
+MA = int(input())
+DIAS[1] = MA
+print(f"El día MARTES se vendieron {MA} productos")
+MI = int(input())
+DIAS[2] = MI
+print(f"El día MIERCOLES se vendieron {MI} productos")
+JU = int(input())
+DIAS[3] = JU
+print(f"El día JUEVES se vendieron {JU} productos")
+VI = int(input())
+DIAS[4] = VI
+print(f"El día VIERNES se vendieron {VI} productos")
