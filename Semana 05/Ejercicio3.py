2fe9cbc9509b6b425e7f29febe6056fc6a313d68
vslopezs18-star <vslopezs18@gmail.com>
2025-10-15 12:33:42 -0600

    .

--- Semana 05/Ejercicio3.py
+++ Semana 05/Ejercicio3.py
@@ -1,5 +1,5 @@
 correo = input()
 
 print(f"Contiene exactamente un @: {correo.count("@")==1}")
-print(f"El @ no está ubicado al inicio del correo: {correo[0]==("@")}")
-print(f"El @ no está ubicado al final del correo: {correo==("@")}")
+print(f"El @ no está ubicado al inicio del correo: {correo[0]!=("@")}")
+print(f"El @ no está ubicado al final del correo: {correo!=("@")}")
