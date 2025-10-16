d0a96b70ebff1c6966b31e60fb7db9b23cf2751f
vslopezs18-star <vslopezs18@gmail.com>
2025-10-16 16:39:35 -0600

    .

--- Semana 05/Control2 Ejercicio 1.py
+++ Semana 05/Control2 Ejercicio 1.py
@@ -1,6 +1,5 @@
 PLATOS_DISPONIBLES = ("Hamburguesa","Hotdog","Pizza","Tacos","Lasaña","Ensalada","Pupusas","Burrito","Alitas de pollo","Papas fritas")
 
 numero_plato = int(input())
-variable1 = numero_plato - 1
 
 print(f"El plato de Alvin: {PLATOS_DISPONIBLES[numero_plato - 1]}")
--- Semana 05/Ejercicio2.py
+++ Semana 05/Ejercicio2.py
@@ -1,10 +1,9 @@
 lista_completaprecios = [3.52, 55.15 , 4.25, 60.25, 5.65, 3.15, 2.65, 70.75, 6.25, 2.55]
 
 plato = int(input())
-variable1 = plato - 1
-
 nuevo_precio = float(input())
-lista_completaprecios[variable1] = nuevo_precio
+
+lista_completaprecios[plato-1] = nuevo_precio
 
 print(f"Los precios actualizados son: {lista_completaprecios}")
 
