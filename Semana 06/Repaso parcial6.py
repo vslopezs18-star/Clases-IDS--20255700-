7e415d56b7cac9d8a6ff54d37f130881abcf8e7c
vslopezs18-star <vslopezs18@gmail.com>
2025-10-16 12:58:21 -0600

    .

---
+++ Semana 06/Repaso parcial6.py
@@ -0,0 +1,14 @@
+uno = int(input())
+dos = int(input())
+tres = int(input())
+cuatro = int(input())
+cinco = int(input())
+
+seis = float(input())
+siete = float(input())
+ocho = float(input())
+nueve = float(input())
+diez = float(input())
+
+puntuacion_total = ((uno*seis) + (dos*siete) + (tres*ocho) + (cuatro*nueve) + (cinco*diez))
+print(puntuacion_total)
