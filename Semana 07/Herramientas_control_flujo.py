213ce4e1a660a979579bbf7006211cb2788e66bb
vslopezs18-star <vslopezs18@gmail.com>
2025-10-23 19:41:51 -0600

    Update Herramientas_control_flujo.py

---
+++ Semana 07/Herramientas_control_flujo.py
@@ -0,0 +1,6 @@
+# if condición: es con una tabulación de espacio hacia adentro donde indica que empieza y termina el bloque de código que se va a ejecutar si la condición es verdadera
+
+nota = float(input("Indique la nota: "))
+
+if nota>=6: 
+    print("Pasó") # Si la nota es mayor o igual a 6, se imprime "Pasó"
