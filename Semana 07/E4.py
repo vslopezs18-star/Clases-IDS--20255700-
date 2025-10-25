f541506def508b07a252018bfeb9c8f54dd3a7ad
vslopezs18-star <vslopezs18@gmail.com>
2025-10-25 13:21:15 -0600

    .

--- Semana 07/E4.py
+++ Semana 07/E4.py
@@ -3,11 +3,11 @@ numero = int(input())
 cont7 = 0
 cont5 = 0
 
-for v in range(numero):
-    lista = int(input())
-    if numero == 7:
+for v in range(numero): # Repite el bloque de código 'numero' veces
+    n = int(input())    # Lee un número entero y después con el if lo compara con 7 y 5 y cuenta cuántas veces aparecen, se le suma 1 al contador correspondiente porque empezaba en 0
+    if n == 7:
         cont7 = cont7 + 1
-    elif numero == 5:
+    if n == 5:
         cont5 = cont5 + 1
 
-print(cont7 and cont5)
+print(cont7, cont5) # La coma separa los dos valores a imprimir
---
+++ Semana 07/E5.py
@@ -0,0 +1,11 @@
+cantidad_combos = int(input())
+
+Pa = int(input())
+Pb = int(input())
+Pc = int(input())
+
+for v in range(cantidad_combos):
+    combos = input()
+    daño_total = 0
+    
+    for ataque in (6):
