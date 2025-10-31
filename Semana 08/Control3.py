2d70c2253a8190387b47b890f8b61056b0a74de6
vslopezs18-star <vslopezs18@gmail.com>
2025-10-31 16:51:23 -0600

    .

--- Semana 08/Control3.py
+++ Semana 08/Control3.py
@@ -15,14 +15,14 @@ while nombre_ingresado.lower() != agente.lower():
 while platosypedidos:
     opción = int(input("1. Creación de platillos, 2.Consulta de platillos y precios, 3.Colocar un pedido, 4. Salir"))
     if opción == 1:
-        platillo.append(input("Ingrese el nombre del platillo a crear: "))
+        platillo.append(input("Ingrese el nombre del platillo a crear: ").lower())
         precios.append(float(input("Ingrese el precio del platillo a crear: ")))
     elif opción == 2:
         if len(platillo) == 0:
             print("Actualmente no hay platillos ingresados.")
         else:
             for v in range(len(platillo)):
-                print(f"{platillo[v]}: ${precios[v]}")
+                print(f"{platillo[v].capitalize()}: ${precios[v]:.2f}")
     elif opción == 3:
         platillo_elegido = input("Indique el nombre del platillo para su orden: ")
         if platillo_elegido.lower() in platillo:
@@ -30,6 +30,6 @@ while platosypedidos:
             print(f"Usted ha elegido {platillo[indice]} con un precio de {precios[indice]}")
         elif platillo_elegido.lower() not in platillo:
             print("Ese platillo no existe")
-        elif opción == 4:
+    elif opción == 4:
             platosypedidos = False
     
