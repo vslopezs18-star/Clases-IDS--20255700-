5fdbdeb3c70edba9b4c1743b68888790a53233e8
vslopezs18-star <vslopezs18@gmail.com>
2025-11-14 12:39:33 -0600

    .

--- Semana 10/estudiantes.py
+++ Semana 10/estudiantes.py
@@ -3,6 +3,7 @@
 def registrar_estudiante(lista_estudiantes):
     """Registra el nombre y carnet de los estudiantes"""
     nombre = input("Nombre: ")
+    # Como se tienen que crear automáticamente los carnets pongo las condiciones para cada caso
     numeros = len(lista_estudiantes) + 1
     if numeros < 10:
         carnet = "S00" + str(numeros)
@@ -10,19 +11,21 @@ def registrar_estudiante(lista_estudiantes):
         carnet = "S0" + str(numeros)
     else:
         carnet = "S" + str(numeros)
-    
+    # Guardo todos los datos en un diccionario para que se agreguen a la lista de estudiantes
     estudiante = {
         "carnet": carnet,
         "nombre": nombre
     }
     
     lista_estudiantes.append(estudiante)
+    # De igual manera pongo un mensaje para que se sepa el carnet correspondiente al estudiante
     print(f"Estudiante registrado: {nombre} con carnet {carnet}.")
 
 # Luego creo la función para mostrar los estudiantes
 
 def mostrar_estudiantes(lista_estudiantes):
     """Muestra a los estudiantes registrados"""
+    # Si no hay estudiantes entonces pongo un mensaje para que lo diga y regrese al inicio
     if len(lista_estudiantes) == 0:
         print("No hay ningún estudiante registrado aún")
         return
--- Semana 10/prestamos.py
+++ Semana 10/prestamos.py
@@ -3,43 +3,44 @@
 def registrar_prestamo(lista_libros, lista_estudiantes, lista_prestamos):
     """Registra los préstamos que se vayan haciendo"""
     carnet = input("Ingrese su carnet: ")
+    # Verifico que el carnet este registrado en la lista de estudiantes, para eso me voy al diccionario en la clave carnet de la lista de estudiantes
     encontrado = False
     for estudiante in lista_estudiantes:
         if estudiante["carnet"] == carnet:
             encontrado = True
             print("El carnet sí está registrado")
-    
+    # Aquí hago saber que el carnet ingresado no existe y vuelve al inicio
     if encontrado == False:
         print("Este carnet no está registrado")
         return
     
     código = input("Ingrese el código del libro: ")
-           
+    # Hago un diccionario vacío para almacenar los códigos de los libros a prestar       
     libro_actual = {}
     for libro in lista_libros:
         if libro["código"] == código:
                 libro_actual = libro
-        
+    # Si no hay libros con el código ingresado entonces muestro un mensaje, pero si sí entonces aviso que se encontró 
     if len(libro_actual) == 0:
         print("El código ingresado no pertenece a ningún libro")
         return
     else:
         print("El libro fue encontrado")
-        
+    # Veo si el estado del libro para mostrar si está prestado o no    
     if libro_actual["disponible"] == False:
         print("El libro actualmente está prestado")
         return
     
         
     fecha = input("Ingrese la fecha del préstamo (ej. DD/MM/AAAA): ")
-    
+    # Hago un diccionario para que todos los datos ingresados se vayan a la lista de los préstamos
     préstamo = {
         "carnet del estudiante": carnet,
         "código del libro": código,
         "fecha": fecha
     }
     lista_prestamos.append(préstamo)
-    
+    # Aquí pongo el libro como ya no disponible
     libro_actual["disponible"] = False
     print("El préstamo fue registrado con éxito.")
     
@@ -47,6 +48,7 @@ def registrar_prestamo(lista_libros, lista_estudiantes, lista_prestamos):
 
 def mostrar_prestamos(lista_prestamos):
     """Muestra los préstamos realizados"""
+    # Verifico si hay préstamos o no
     if len(lista_prestamos) == 0:
         print("No hay préstamos actualmente.")
         return
